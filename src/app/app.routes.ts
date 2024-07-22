9d3a2d9faef7a70ff95bcacbcfe626705104e026
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-07-22 15:20:41 -0500

    chore: create contact component and add it in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LayoutHeaderFooterComponent } from '@shared/components/layout-header-fo
 import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 import { HomeComponent } from '@info/pages/home/home.component';
 import { StoreComponent } from '@products/pages/store/store.component';
+import { ContactComponent } from '@info/pages/contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -22,6 +23,10 @@ export const routes: Routes = [
       {
         path: 'about',
         component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
       },
       {
         path: 'product/:id',
