b741e9ef79abae27c54fad1d360673af32427151
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-07-22 02:19:27 -0500

    chore: create component card-category

---
+++ src/app/domains/products/components/card-category/card-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-category.component.html',
+  styleUrl: './card-category.component.css'
+})
+export class CardCategoryComponent {
+
+}
