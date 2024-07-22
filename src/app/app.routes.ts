9a3f8e4419577c705abeb7bf0f572553b71400e9
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-07-22 01:42:53 -0500

    chore: add files  componentes home, banner, list-categories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from '@products/pages/list/list.component';
+import { ListComponent } from '@products/components/list/list.component';
 import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 import { LayoutHeaderFooterComponent } from '@shared/components/layout-header-footer/layout-header-footer.component';
 import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
+import { HomeComponent } from '@info/pages/home/home.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
     children: [
       {
         path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'store',
         component: ListComponent,
       },
       {
