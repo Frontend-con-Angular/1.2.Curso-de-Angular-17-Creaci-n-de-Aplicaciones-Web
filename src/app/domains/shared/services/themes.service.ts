9f08305d5739dd8b8bc8b10cc485b92d6a61b96c
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-07-24 02:29:29 -0500

    fix(theme service): change the way to declarate window and document

--- src/app/domains/shared/services/themes.service.ts
+++ src/app/domains/shared/services/themes.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,16 +8,22 @@ import { Injectable, signal } from '@angular/core';
 export class ThemesService {
   isDarkMode = signal(false);
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
-  readUserPreference(){
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-    document.body.classList.toggle('dark', prefersDarkScheme.matches);
-    this.isDarkMode.set(prefersDarkScheme.matches);
+  readUserPreference() {
+    if (isPlatformBrowser(this.platformId)) {
+      const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+      if (typeof document !== 'undefined'){
+        document.body.classList.toggle('dark', prefersDarkScheme.matches);
+        this.isDarkMode.set(prefersDarkScheme.matches);
+      }
+    }
   }
 
-  toggleDarkMode(){
-    this.isDarkMode.update(state=>!state);
-    document.body.classList.toggle('dark', this.isDarkMode());
+  toggleDarkMode() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDarkMode.update(state => !state);
+      document.body.classList.toggle('dark', this.isDarkMode());
+    }
   }
 }
